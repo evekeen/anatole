eb3726b5e659541ec38bd1a5d751a441b358072a
Alexander Ivkin <a@ivkin.dev>
2025-04-10 21:35:33 -0400

    fixed links

--- assets/js/link-target.js
+++ assets/js/link-target.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const links = Array.prototype.slice.call(document.querySelectorAll('.post__content a'), 0);
+  const links = Array.prototype.slice.call(document.querySelectorAll('.post__content.full-post a'), 0);
   if (links.length < 1) return;
   links.forEach((link) => {
     link.target = '_blank';
